fc339c749eb36504d77f1f3018da33852e94a1cb
Allison Eichhorn <allisoneichhorn@gmail.com>
2019-10-07 23:00:31 -0400

    Create about.php

---
+++ about.php
@@ -0,0 +1,33 @@
+<?php include("top.html"); ?>
+	
+	<div id = "about">
+	
+		<h1>About</h1>
+	
+		<div id = "intro">
+			<p>Over 15 years of experience in the community, school, charity, & private events. Let me help you create a one of a kind event!</p>
+		</div>
+	
+		<div id = "profile">
+			<img src= "Profile.JPG" alt = "profile"/>
+		</div>
+	
+		<div id = "bio">
+			<p>As a young child, I loved to help my grandmother with many Sunday family dinners. Entertaining brings me joy.  I love to think about the person or the cause being honored and incorporate that into a special day. The skill of organization is one of my strengths.  I have a passion for color and décor and love to add those personal touches to all of my events.  
+			<br><br>
+			In the first chapter of my career, I worked in Finance and Accounting for an Investment Banking Firm. For the past two decades, I have been a volunteer in our community. 
+			<br><br>
+			I’m a founding member of our Education Foundation and have helped create and organize numerous charitable events and after school programs for over six years.
+			<br><br>
+			I initiated and ran our Elementary School Harvest Festival for eight years and many graduation events.  I envisioned and ran our Elementary and High School Cultural Awareness and Career Fair events for over a decade.  
+			<br><br>
+			I love order and planning out all the elements of an event as if figuring out pieces of a puzzle.  I have a passion for flowers and love to create centerpieces to coordinate with the theme of the party and color scheme.  
+			<br><br>
+			Along the way, I’ve developed my photographic skills in capturing the individual of honor and all the aspects of the party.  To round out my skills, cooking and baking are one of my many true loves.  I truly enjoy preparing personalized appetizers, entrées and baked goods for the person or event at hand.
+			<br><br>
+			I look forward to incorporating all of my passions in making a truly exceptional event for you and your family.
+			</p>
+		</div>
+	</div>
+		
+<?php include("bottom.html"); ?>
