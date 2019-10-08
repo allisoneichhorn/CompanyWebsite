5e69c1141588f1c5dc0aa81a4db52d51da878d8a
Allison Eichhorn <allisoneichhorn@gmail.com>
2019-10-07 23:04:49 -0400

    Create home.php

---
+++ home.php
@@ -0,0 +1,11 @@
+<?php include("top.html"); ?>
+		
+		<div>
+			<img src= "venue2.JPG" alt = "venue"/>
+		</div>
+		
+		<div class="contentBox">
+			<p>Exceptional Service from beginning to end!</p>
+		</div>
+
+<?php include("bottom.html"); ?>
