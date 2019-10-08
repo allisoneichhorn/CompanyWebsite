4e6d6b66b14b368d0c1aee5517bd15a12a7271e0
Allison Eichhorn <allisoneichhorn@gmail.com>
2019-10-07 23:05:19 -0400

    Create lovenotes.php

---
+++ lovenotes.php
@@ -0,0 +1,103 @@
+<?php include("top.html"); ?>
+
+	<div id = "lovenotes">
+		<h1>Love Notes</h1>
+
+		<div>
+			<img src= "couple3.jpg" alt = "couple"/>
+		</div>
+
+		<br>
+		<br>
+		<br>
+
+		<blockquote>
+		"This event planner is absolutely amazing! She catered to everything that I wanted and needed for my special day. Colleen designed my entire wedding and it came out better than I could have ever dreamed. I recommend Colleen over anyone else. Will definitely be coming back!"
+		<br>
+		<br>
+
+		<div class ="reviewer">- Allison E</div>
+
+		</blockquote>
+
+		<blockquote>
+		"So glad we hired Cellebrations by Colleen for our wedding! The DJ we initially met with and hired had to back out, but the owner Eric handled the situation perfectly, and matched us with our new DJ, Tony E. On the day, Tony was great! We've been to other weddings where the DJs are very chatty and take over the reception. We wanted a giant dance party and that's exactly what we got."
+		<br>
+		<br>
+
+		<div class ="reviewer">- Rachel P</div>
+
+		</blockquote>
+
+		<blockquote>
+		"Colleen threw in an extra $700 worth of work on my friend's wedding flowers for free. They did a beautiful job with our small budget and we couldn't feel anymore blessed. You can tell they are in the business because they love it, not for just profit. I highly recommend this place. Their work is amazing and the people are genuine."
+		<br>
+		<br>
+
+		<div class ="reviewer">- Lilly N</div>
+
+		</blockquote>
+
+		<blockquote>
+		"I'm your loyal customer from now for FOREVER!!
+		Ask Colleen anythin, even late evening time she will make and deliver it!!!"
+
+		<br>
+		<br>
+
+		<div class ="reviewer">- Allen S</div>
+
+		</blockquote>
+
+		<blockquote>
+		"Colleen and her team did our wedding recently and we couldn't be happier! Everyone at our wedding commented on how beautiful the flowers were. They really did an outstanding job. We came with a budget of course but what they delivered was way better than I ever expected!!!"
+		<br>
+		<br>
+
+		<div class ="reviewer">- Rita G</div>
+
+		</blockquote>
+
+		<blockquote>
+		"Colleen and her staff were so amazing. My husband and I both agreed that she was worth every penny. More importantly, she was flexible with our phone appointments considering that we lived out of state, and she made every effort to accommodate our busy schedule."
+		<br>
+		<br>
+
+		<div class ="reviewer">- Lynn N</div>
+
+		</blockquote>
+
+		<blockquote>
+		"Colleen was our saving grace. We had decided to go with Colleen based on the glowing recommendation of one of my very best friends (in addition to the fact that Colleen was incredibly kind and sweet when we met with her for our first consultation). It was one of the best decisions we made." 
+		<br>
+		<br>
+
+		<div class ="reviewer">- Mindy N</div>
+
+		</blockquote>
+		<br>
+
+		<form action="submit_review.php" method="post">
+			<fieldset>
+				<legend>Submit Your Love Note Here</legend>
+
+				<strong>Name: </strong><input type="text" name="name" size="16" />
+			
+				<br>
+				<br>
+
+				<strong>Love Note: </strong>
+				<textarea rows = "4" cols = "50" name="review"></textarea>
+
+				<br>
+				<br>
+                        
+				<input type="submit" value="Submit">
+			</fieldset>
+		</form>
+
+		<br>
+		<br>
+	</div>
+
+<?php include("bottom.html"); ?>
